1e69ff2e9506a29cf7ae6a614f508fb6f6ba5945
Eddy Ma <eddy.ma@smsmt.com>
2017-01-29 17:18:25 +1000

    Add Ninject as IoC

---
+++ src/Mediator.Net.Ninject/NinjectDependancyScope.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ninject;
+
+namespace Mediator.Net.Ninject
+{
+    public class NinjectDependancyScope : IDependancyScope
+    {
+        private readonly StandardKernel _kernal;
+
+        public NinjectDependancyScope(StandardKernel kernal)
+        {
+            _kernal = kernal;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>()
+        {
+            return _kernal.Get<T>();
+        }
+
+        public object Resolve(Type t)
+        {
+            return _kernal.GetService(t);
+        }
+
+        public IDependancyScope BeginScope()
+        {
+            return new NinjectDependancyScope(_kernal);
+        }
+    }
+}
---
+++ src/Mediator.Net.Ninject/NinjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Ninject;
+
+namespace Mediator.Net.Ninject
+{
+    public static class NinjectExtensions
+    {
+        public static void Configure(StandardKernel kernal, MediatorBuilder builder)
+        {
+            kernal.Bind<MediatorBuilder>().ToConstant(builder);
+            kernal.Bind<IDependancyScope>().ToMethod(x => new NinjectDependancyScope(kernal));
+        }
+    }
+}
