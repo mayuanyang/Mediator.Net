646c737df51facb83a5e13e981aa02a3bbab0e7d
Eddy Ma <eddy.ma@octopus.com>
2018-05-27 20:04:44 +1000

    Update the build

--- update-version.ps1
+++ update-version.ps1
@@ -1,3 +1,11 @@
+if ($env:APPVEYOR_REPO_TAG -eq "true" -And $env:APPVEYOR_REPO_BRANCH -eq "master")
+{
+    Update-AppveyorBuild -Version "$($env:APPVEYOR_REPO_TAG_NAME)"
+}
+else
+{
+    Update-AppveyorBuild -Version "$env:APPVEYOR_REPO_BRANCH-$($env:APPVEYOR_BUILD_VERSION)-ci$($env:APPVEYOR_BUILD_NUMBER)"
+}
 $configFiles = Get-ChildItem . *.csproj -rec
 $versionString = "<PackageVersion>" + $env:APPVEYOR_BUILD_VERSION + "</PackageVersion>"
 foreach ($file in $configFiles)
