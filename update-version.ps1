224ef0983bb0456a19521c75465c61a2501b1211
Eddy Ma <eddy.ma@octopus.com>
2018-05-27 22:05:07 +1000

    Enhence build

--- update-version.ps1
+++ update-version.ps1
@@ -1,11 +1,14 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master")
 {
+	write-host "It is master branch, update the version as a actual release"
 	$ver = "$($env:APPVEYOR_BUILD_VERSION)"
 	$verForRelease = $ver.SubString(0,$ver.lastIndexOf("."))
+	write-host "The version is now $verForRelease"
     Update-AppveyorBuild -Version "$verForRelease"
 }
 else
 {
+	write-host "non master branch"
     Update-AppveyorBuild -Version "$($env:APPVEYOR_BUILD_VERSION)-$($env:APPVEYOR_REPO_BRANCH)$($env:APPVEYOR_BUILD_NUMBER)"
 }
 $configFiles = Get-ChildItem . *.csproj -rec
