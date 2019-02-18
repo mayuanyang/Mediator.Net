63176c590d84b540f9221ea3ef4d76c7b3adfbeb
Eddy Ma <ocean_ltd@hotmail.com>
2019-02-18 04:58:50 -0800

    update version logic

--- update-version.ps1
+++ update-version.ps1
@@ -3,6 +3,11 @@ if ($env:APPVEYOR_REPO_BRANCH -eq "master")
 	write-host "It is master branch, update the version as a actual release"
 	$ver = "$($env:APPVEYOR_BUILD_VERSION)"
 	$verForRelease = $ver.SubString(0,$ver.lastIndexOf("."))
+	$parts = $verForRelease.Split(".")
+	$lastPart = [int]$parts[2]
+	$lastPart = $lastPart + 1
+	$verForRelease = $verForRelease.SubString(0,$verForRelease.lastIndexOf(".")) 
+	$verForRelease = $verForRelease + "." + $lastPart
 	write-host "The version is now $verForRelease"
     Update-AppveyorBuild -Version "$verForRelease"
 }
