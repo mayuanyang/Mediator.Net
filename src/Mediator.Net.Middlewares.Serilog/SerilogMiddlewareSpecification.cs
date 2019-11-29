fc05db26edc576828f87cb4b8a2609a6fee57fc5
BERTON.L <berton@sjfood.com>
2019-11-29 15:50:52 -0800

    track stack in pipeline

--- src/Mediator.Net.Middlewares.Serilog/SerilogMiddlewareSpecification.cs
+++ src/Mediator.Net.Middlewares.Serilog/SerilogMiddlewareSpecification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Mediator.Net.Context;
@@ -75,7 +76,8 @@ namespace Mediator.Net.Middlewares.Serilog
         public Task OnException(Exception ex, TContext context)
         {
             _logger.Error(ex, "Error has occured: {@Exception}");
-            throw ex;
+            ExceptionDispatchInfo.Capture(ex).Throw();
+            return Task.WhenAll();
         }
     }
 }
--- src/Mediator.Net/Pipeline/EmptyPipeSpecification.cs
+++ src/Mediator.Net/Pipeline/EmptyPipeSpecification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Mediator.Net.Context;
@@ -32,7 +33,8 @@ namespace Mediator.Net.Pipeline
         
         public Task OnException(Exception ex, TContext context)
         {
-            throw ex;
+            ExceptionDispatchInfo.Capture(ex).Throw();
+            return Task.WhenAll();
         }
     }
 }
