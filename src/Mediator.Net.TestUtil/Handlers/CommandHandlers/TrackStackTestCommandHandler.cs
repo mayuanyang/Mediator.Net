75f167451cb8e811a5f88bf70a4a46cbdf0d90fb
BERTON.L <berton@sjfood.com>
2019-11-29 17:26:07 -0800

    add test

---
+++ src/Mediator.Net.TestUtil/Handlers/CommandHandlers/TrackStackTestCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Mediator.Net.Context;
+using Mediator.Net.Contracts;
+using Mediator.Net.TestUtil.Messages;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mediator.Net.TestUtil.Handlers.CommandHandlers
+{
+    public class TrackStackTestCommandHandler : ICommandHandler<TrackStackTestCommand>
+    {
+        public Task Handle(IReceiveContext<TrackStackTestCommand> context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
