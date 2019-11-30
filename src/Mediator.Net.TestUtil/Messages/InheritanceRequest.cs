e492049688208972989c5154e3fef01079667b9f
Eddy Ma <ocean_ltd@hotmail.com>
2019-11-30 00:50:08 -0800

    Add tests

---
+++ src/Mediator.Net.TestUtil/Messages/InheritanceRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mediator.Net.Contracts;
+
+namespace Mediator.Net.TestUtil.Messages
+{
+    public class InheritanceRequest : IRequest
+    {
+        public Guid Id { get; }
+
+        public string Content { get; set; }
+
+        public InheritanceRequest(Guid id)
+        {
+            Id = id;
+        }
+    }
+
+    public class InheritanceResponse : IResponse
+    {
+        public Guid Id { get; }
+
+        public string Content { get; set; }
+
+        public InheritanceResponse(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
