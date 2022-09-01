9e34cfbca50fe421ccf8a92dcac69fbb3ff00d4c
Eddy Ma <ocean_ltd@hotmail.com>
2022-09-01 15:52:21 -0700

    remove old project

--- src/Mediator.Net.Sample.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Mediator.Net.Sample.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- src/Mediator.Net.Sample.Web/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Mediator.Net.Sample.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
